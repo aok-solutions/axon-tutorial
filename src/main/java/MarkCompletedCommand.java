cff785f9f29a82427d298ea021839f0745ef09fb
Ayanga Okpokowuruk <ayanga.okpokowuruk@manheim.com>
2015-08-10 14:11:15 -0500

    Implemented Commands and Events

---
+++ src/main/java/MarkCompletedCommand.java
@@ -0,0 +1,14 @@
+import org.axonframework.commandhandling.annotation.TargetAggregateIdentifier;
+
+public class MarkCompletedCommand {
+    @TargetAggregateIdentifier
+    private final String todoId;
+
+    public MarkCompletedCommand(String todoId) {
+        this.todoId = todoId;
+    }
+
+    public String getTodoId() {
+        return todoId;
+    }
+}
