60003245145d3589cee4b9f51be36b833f27a1c3
Ayanga Okpokowuruk <ayanga.okpokowuruk@manheim.com>
2015-08-10 14:20:16 -0500

    Implemented the Aggregate

--- src/main/java/ToDoItem.java
+++ src/main/java/ToDoItem.java
@@ -1,4 +1,27 @@
+import org.axonframework.commandhandling.annotation.CommandHandler;
+import org.axonframework.eventhandling.annotation.EventHandler;
 import org.axonframework.eventsourcing.annotation.AbstractAnnotatedAggregateRoot;
+import org.axonframework.eventsourcing.annotation.AggregateIdentifier;
 
 public class ToDoItem extends AbstractAnnotatedAggregateRoot{
+    @AggregateIdentifier
+    private String id;
+
+    public ToDoItem(){
+    }
+
+    @CommandHandler
+    public ToDoItem(CreateToDoItemCommand command) {
+        apply(new ToDoItemCreatedEvent(command.getTodoId(), command.getDescription()));
+    }
+
+    @EventHandler
+    public void on(ToDoItemCreatedEvent event){
+        this.id = event.getTodoId();
+    }
+
+    @CommandHandler
+    public void markCompleted(MarkCompletedCommand command) {
+        apply(new ToDoItemCompletedEvent(id));
+    }
 }
